765772b78dad62eb798a1470ed32c2385a43f77f
Max Rubs <38310511+MaxRubshik@users.noreply.github.com>
2018-07-24 18:58:09 +0300

    update. Making result page

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template #add render_template to adding list
+from flask import Flask, render_template, request #add render_template to adding list
 from vsearch import search4letters
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello() -> str:
     return 'Hello world from Flask!'
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
-    return str(search4letters('life, the universe, and everything',
-                              'eiru,!'))
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    the_title = title 
+    the_results = results search4letters
+    
+    return render_template('results.html',
+                           the_title='Here are your results:')
+
+
 @app.route('/entry') # Создать новый URL
 def entry_page() -> 'html':
     return render_template('entry.html',
-                           the_title='Welcome to search4letters on the web!')
-app.run()
+                           the_title='Welcome to search4letters my webpage!')
+app.run(debug=True)
