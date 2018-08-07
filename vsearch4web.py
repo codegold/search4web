af2b6ad21a7c85c6c7022a48e8ca848da65d93f9
Max Rubs <38310511+MaxRubshik@users.noreply.github.com>
2018-08-07 16:23:16 +0300

    Add files via upload
    start page redirect to search

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,25 +1,26 @@
 from flask import Flask, render_template, request #add render_template to adding list
-from vsearch import search4letters #just testing
+from vsearch import search4letters 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
+#def hello() -> '302':
+ #   return redirect('/entry')
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    the_title = title 
-    the_results = results search4letters
-    
+    title = 'Here your results: '
+    results = str(search4letters(phrase, letters))
     return render_template('results.html',
-                           the_title='Here are your results:')
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
 
-
+@app.route('/')
 @app.route('/entry') # Создать новый URL
 def entry_page() -> 'html':
     return render_template('entry.html',
-                           the_title='Welcome to search4letters my webpage!')
+                           the_title='Welcome to search4letters MY OWN webpage!')
 app.run(debug=True)
