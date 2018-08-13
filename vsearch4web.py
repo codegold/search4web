a4e23c0576ae59a124d46e2c562d7eaa33b18ad6
Max Rubs <38310511+MaxRubshik@users.noreply.github.com>
2018-08-13 11:35:56 +0300

    Add files via upload
    'улучшено viewlog'

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request #add render_template to adding list
+from flask import Flask, render_template, request, escape 
 from vsearch import search4letters 
 
 app = Flask(__name__)
-
-#def hello() -> '302':
+def log_request(req: 'flask_request', res: str) -> None: #page 286
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, end='|')
+ #def hello() -> '302':
  #   return redirect('/entry')
-
+  
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here your results: '
     results = str(search4letters(phrase, letters))
+    log_request(request, results) #page 286
     return render_template('results.html',
                            the_phrase=phrase,
                            the_letters=letters,
@@ -23,5 +26,21 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters MY OWN webpage!')
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                          the_title='View Log',
+                          the_row_titles=titles,
+                          the_data=contents,)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
